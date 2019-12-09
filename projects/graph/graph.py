e0cf1839b1679659535aad3ff4b3a362aadb5bd9
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-12-09 11:48:56 -0500

    add vertex and add edge working

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -13,26 +13,34 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            if v1 in self.vertices:
+                raise IndexError(f"{v2} is not a valid vertex in Graph")
+            else:
+                raise IndexError(f"{v1} is not a valid vertex in Graph")
+
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        return self.vertices[vertex_id]
 
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        queue = Queue()
+        queue.enqueue(starting_vertex)
 
     def dft(self, starting_vertex):
         """
