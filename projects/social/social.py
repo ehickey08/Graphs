2f42b3ce9832df3f80b6b0942c0d8d3f7222b244
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-12-11 09:17:24 -0500

    generating random friends for social - classwork

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,8 +1,18 @@
+import random
+import sys
+
+sys.path.append('..')
+
+from graph.util import Queue
 
 
 class User:
     def __init__(self, name):
         self.name = name
+
+    def __repr__(self):
+        return self.name
+
 
 class SocialGraph:
     def __init__(self):
@@ -16,7 +26,8 @@ class SocialGraph:
         """
         if user_id == friend_id:
             print("WARNING: You cannot be friends with yourself")
-        elif friend_id in self.friendships[user_id] or user_id in self.friendships[friend_id]:
+        elif friend_id in self.friendships[user_id] or user_id in \
+                self.friendships[friend_id]:
             print("WARNING: Friendship already exists")
         else:
             self.friendships[user_id].add(friend_id)
@@ -30,7 +41,7 @@ class SocialGraph:
         self.users[self.last_id] = User(name)
         self.friendships[self.last_id] = set()
 
-    def populate_graph(self, numUsers, avgFriendships):
+    def populate_graph(self, num_users, avg_friendships):
         """
         Takes a number of users and an average number of friendships
         as arguments
@@ -47,8 +58,21 @@ class SocialGraph:
         # !!!! IMPLEMENT ME
 
         # Add users
+        for i in range(num_users):
+            self.add_user(f"User {i}")
 
         # Create friendships
+        possible_friendships = []
+        for userID in self.users:
+            for friendID in range(userID + 1, self.last_id + 1):
+                possible_friendships.append((userID, friendID))
+        random.shuffle(possible_friendships)
+
+        total_friendships = avg_friendships * num_users // 2
+
+        for i in range(total_friendships):
+            friendship = possible_friendships[i]
+            self.add_friendship(friendship[0], friendship[1])
 
     def get_all_social_paths(self, user_id):
         """
@@ -61,12 +85,14 @@ class SocialGraph:
         """
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
+
         return visited
 
 
 if __name__ == '__main__':
     sg = SocialGraph()
     sg.populate_graph(10, 2)
+    print(sg.users)
     print(sg.friendships)
     connections = sg.get_all_social_paths(1)
     print(connections)
